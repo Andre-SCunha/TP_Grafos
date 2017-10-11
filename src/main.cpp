517a6b471bcb1fb5f50aaae2359f2c0dd8a85d41
Andre-SCunha <brayils.andre@gmail.com>
2017-10-11 17:15:07 -0300

    Reading Input

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
 #include "Graph.h"
+#include <list>
 #define MAX(a,b) (((a)>(b))?(a):(b))
 
 using namespace std;
 
 int main(){
 
-    Graph myGraph(10);
-    cout << myGraph.n << " " << myGraph.m << endl;
-    myGraph.addEdge(0,1,0);
-    myGraph.addEdge(9,1,7);
-    myGraph.addEdge(2,5,6);
+//Reading input
+    list<int> v, u;
+    int _v, _u, max = 0, aux, m = 0;
+    list<float> w;
+    float _w;
+    char c;
+    cin >>_v >> c >> _u >> c >> _w;
+    while (_v!=0 || _u!=0 || _w!=0){
+        v.push_back(_v);
+        u.push_back(_u);
+        w.push_back(_w);
+        m++;
+
+        aux = MAX(_v,_u);
+        max = MAX(max,aux);
+        cin >>_v >> c >> _u >> c >> _w;
+    }
+
+//Making Graph
+    Graph myGraph(max+1);
+
+    for (int i = 0; i < m; i++){
+        myGraph.addEdge(v.front(), u.front(), w.front());
+        v.pop_front(); u.pop_front(); w.pop_front();
+    }
+
+//Printing Graph
     cout << myGraph.n << " " << myGraph.m << endl;
     for (int i=0; i<myGraph.n; i++){
         cout << myGraph.V[i]->id;
