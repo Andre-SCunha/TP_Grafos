41459e5685a5b66d3ff603a5645cb036d8ce1fd7
Andre-SCunha <brayils.andre@gmail.com>
2017-10-12 11:55:37 -0300

    Using priority queue (binary tree)

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <set>
 #include <vector>
+#include <limits>
 #define MAX(a,b) (((a)>(b))?(a):(b))
 
 using namespace std;
@@ -20,15 +21,12 @@ struct Pair
 void simDij(Graph& G, int source, vector<Pair> &inf);
 
 int main(){
-
 //Reading input
-    list<int> v, u;
-    int _v, _u, max = 0, aux, m = 0;
-    list<float> w;
-    float _w;
-    char c;
-    cin >>_v >> c >> _u >> c >> _w;
-    while (_v!=0 || _u!=0 || _w!=0){
+    list<int> v, u, w;
+    int _v, _u, _w, max = 0, aux, m = 0;
+    char eater;
+    cin >>_v >> eater >> _u >> eater >> _w;
+    while ((_v!=0) || (_u!=0) || (_w!=0)){
         v.push_back(_v);
         u.push_back(_u);
         w.push_back(_w);
@@ -36,9 +34,8 @@ int main(){
 
         aux = MAX(_v,_u);
         max = MAX(max,aux);
-        cin >>_v >> c >> _u >> c >> _w;
+        cin >>_v >> eater >> _u >> eater >> _w;
     }
-
 //Making Graph
     Graph myGraph(max+1);
 
@@ -54,79 +51,94 @@ int main(){
         inf[i].id = i;
         inf[i].influence = 0;
     }
+
     for (int i = 0; i < myGraph.n; i++){
         if(myGraph.V[i]->connected)
             simDij(myGraph, i, inf);
     }
+
     sort(inf.begin(), inf.end(), Pair::compare);
     for (auto i: inf)
         if(myGraph.V[i.id]->connected)
             cout << i.id << ':' << i.influence << ",";
     cout << endl;
 
-//Printing Graph
-    cout << myGraph.n << " " << myGraph.m << endl;
-    for (int i=0; i<myGraph.n; i++){
-        cout << myGraph.V[i]->id;
-        for (pEdge e : myGraph.V[i]->adj){
-            cout << endl << "\t" << e->w << "," << e->u->id << "," << e->v->id;
-        }
-        cout << endl;
-    }
-
     return 0;
 }
 
 void simDij(Graph& G, int source, vector<Pair> &inf){
 
-    set<int> Q;
-    vector<float> dist(G.n), prev(G.n);
-    int max, v;
-    float aux;
+    struct SimVertex
+    {
+        int id, prev;
+        float dist;
+    };
+    struct pVertexComp
+    {
+        bool operator() (SimVertex* a, SimVertex* b){
+            if(a->dist==b->dist)
+                return a->id < b->id;
+            return a->dist < b->dist;
+        }
+    };
+    set<SimVertex*, pVertexComp> distanceQ;
+    vector<SimVertex> nodes(G.n);
+    SimVertex* first;
+    int v, dest_id, dest_dist;
+    float aux =  std::numeric_limits<float>::infinity();
 
     for (int i = 0; i < G.n; i++){
-        dist[i] = 100000;
-        prev[i] = -1;
-        Q.insert(Q.end(), i);
+        if (i!=source){
+            nodes[i].id = i;
+            nodes[i].dist = aux;
+            nodes[i].prev = -1;
+        } else{
+            nodes[i].id = i;
+            nodes[i].dist = 0;
+            nodes[i].prev = -1;
+        }
+        distanceQ.insert(distanceQ.end(), &nodes[i]);
     }
 
-    dist[source] = 0;
-
-//Modified Dijkstra
-    while(!Q.empty()){
-    //Finding max dist element
-        max = *Q.begin();
-        for (set<int>::iterator it=Q.begin(); it!=Q.end(); ++it){
-            if(dist[*it] < dist[max])
-                max = *it;
-        }
+//Dijkstra
+    while(!distanceQ.empty()){
+    //Finding min dist element
+        first = *distanceQ.begin();
+        dest_id = first->id;
+        dest_dist = first->dist;
     //Removing from the "not reached yet"
-        Q.erase(max);
+        distanceQ.erase(distanceQ.begin());
     //Updating neighbors best similarity
-        for (pEdge e : G.V[max]->adj){
-            v = (e->u->id == max)?e->v->id:e->u->id;
+        for (pEdge e : G.V[dest_id]->adj){
+        //Getting neighbor id
+            v = (e->u->id == dest_id)?e->v->id:e->u->id;
         //If element still in Q
-            if(Q.find(v) != Q.end()){
-                aux = dist[max] + e->w;
-                if(aux < dist[v]){
-                    dist[v] = aux;
-                    prev[v] = max;
+            set<SimVertex*, pVertexComp>::iterator nei = distanceQ.find(&nodes[v]);
+            if(nei != distanceQ.end()){
+                aux = dest_dist + e->w;
+            //Updating distanceQ and node.dist
+                if(aux < nodes[v].dist){
+                //Removing from set
+                    distanceQ.erase(nei);
+                //Updating values
+                    nodes[v].dist = aux;
+                    nodes[v].prev = dest_id;
+                //Re-Inserting on set
+                    distanceQ.insert(&nodes[v]);
                 }
             }
         }
     }
 
-//Printing Solution
+//Paths computation
     for (int i = 0; i < G.n; i++){
         if(G.V[i]->connected){
             int j = i;
             while (j != source){
-                inf[j].influence++;//
-                cout << j << " ";
-                j = prev[j];
+                inf[j].influence++;
+                j = nodes[j].prev;
             }
-            inf[source].influence++;//
-            cout << source << endl;
+            inf[source].influence++;
         }
     }
 
